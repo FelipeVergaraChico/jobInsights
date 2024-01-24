7eb0f00c5708cdcc7d08f6136bec44910328f96a
Felipe Vergara <felipe.vergara.chico@gmail.com>
2024-01-24 18:06:25 -0300

    req 6,7

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -5,12 +5,22 @@ from src.insights.jobs import ProcessJobs
 class ProcessSalaries(ProcessJobs):
     def __init__(self):
         super().__init__()
+        max_salary = int()
+        min_salary = int()
 
     def get_max_salary(self) -> int:
-        pass
+        salaries = [int(job['max_salary'])
+                    for job in self.jobs_list if job["max_salary"].isdigit()]
+        max_salary = max(list(salaries))
+        self.max_salary = max_salary
+        return self.max_salary
 
     def get_min_salary(self) -> int:
-        pass
+        salaries = [int(job['min_salary'])
+                    for job in self.jobs_list if job["min_salary"].isdigit()]
+        min_salary = min(list(salaries))
+        self.min_salary = min_salary
+        return self.min_salary
 
     def matches_salary_range(self, job: Dict, salary: Union[int, str]) -> bool:
         pass
